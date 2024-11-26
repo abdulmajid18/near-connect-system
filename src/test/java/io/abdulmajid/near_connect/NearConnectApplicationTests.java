9054fde8d9a5f921853600d07f0a2f1424a466dc
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-11-26 16:05:26 +0000

    project setup

---
+++ src/test/java/io/abdulmajid/near_connect/NearConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.abdulmajid.near_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
