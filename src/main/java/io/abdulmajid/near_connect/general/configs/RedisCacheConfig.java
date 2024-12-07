ffe0e482d6afe082cc89e2ad773b70a295d8ce3f
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-12-07 12:50:39 +0000

    switch from stomp to generic websocket

--- src/main/java/io/abdulmajid/near_connect/general/configs/RedisCacheConfig.java
+++ src/main/java/io/abdulmajid/near_connect/general/configs/RedisCacheConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.time.Duration;
 
@@ -14,9 +17,18 @@ import java.time.Duration;
 public class RedisCacheConfig {
 
     @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("localhost");
+        config.setPort(6379);
+        config.setPassword(RedisPassword.of("password"));
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .prefixCacheNameWith(this.getClass().getPackageName() + ".")
+                .prefixCacheNameWith("locationHistoryCache")
                 .entryTtl(Duration.ofHours(1))
                 .disableCachingNullValues();
 
--- src/main/java/io/abdulmajid/near_connect/websocket/configs/WebSocketConfig.java
+++ src/main/java/io/abdulmajid/near_connect/websocket/configs/WebSocketConfig.java
@@ -1,28 +1,29 @@
 package io.abdulmajid.near_connect.websocket.configs;
 
+import io.abdulmajid.near_connect.websocket.services.MyWebSocketHandler;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableStompBrokerRelay("/topic", "/queue");
-//                .setRelayHost("localhost")
-//                .setRelayPort(61613)
-//                .setClientLogin("guest")
-//                .setClientPasscode("guest");
-        config.setApplicationDestinationPrefixes("/app");
+    private final CacheManager cacheManager;
+
+    public WebSocketConfig(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MyWebSocketHandler(cacheManager), "/ws")
+                .addInterceptors(new WebSocketHandShakeInterceptor())
                 .setAllowedOriginPatterns("*");
     }
+
+
 }
