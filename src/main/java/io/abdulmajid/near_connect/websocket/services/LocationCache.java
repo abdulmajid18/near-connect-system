397f56d347ad0e2888efc332d1a9b27534872838
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-01-05 01:53:48 +0000

    add locationcache fallback

--- src/main/java/io/abdulmajid/near_connect/websocket/services/LocationCache.java
+++ src/main/java/io/abdulmajid/near_connect/websocket/services/LocationCache.java
@@ -12,8 +12,12 @@ public class LocationCache {
 
     private final CacheManager cacheManager;
 
-    public LocationCache(CacheManager cacheManager) {
+    private final LocationService locationService;
+
+
+    public LocationCache(CacheManager cacheManager, LocationService locationService) {
         this.cacheManager = cacheManager;
+        this.locationService = locationService;
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -43,8 +47,21 @@ public class LocationCache {
         Cache cache = cacheManager.getCache(CacheName.LOCATION_CACHE.getCacheName());  // Get the cache by name
         if (cache != null) {
             Cache.ValueWrapper valueWrapper = cache.get(userId);  // Retrieve the object by key
-            return valueWrapper != null ? (LocationDTO) valueWrapper.get() : null;
+            return valueWrapper != null ? (LocationDTO) valueWrapper.get() : getLocationWithFallback(userId);
         }
         return null;
+    }
+
+    private LocationDTO getLocationWithFallback(String userId) {
+        LocationDTO cachedLocation = getLocationHistoryFromCache(userId);
+        if (cachedLocation == null) {
+            // Fallback to database
+            LocationDTO dbLocation = locationService.getLatestLocation(userId);
+            if (dbLocation != null) {
+                cacheLocation(userId, dbLocation);
+                return dbLocation;
+            }
+        }
+        return cachedLocation;
     }
 }
